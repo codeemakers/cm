571f91a47e1eaa4f07ad35f48943b620499fb098
codeemakers <codemakersolutions@gmail.com>
2024-10-08 17:27:20 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
+import ReactGA from "react-ga"
 import Maintenance from "./components/UI/maintenance";
 import Layout from "./components/Layout/Layout"
+
+const TRACKING_ID = "G-ZVRLRF2HCH"
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return <>
